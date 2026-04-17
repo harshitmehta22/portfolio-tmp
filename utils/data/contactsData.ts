6fb3fa13c1acdcf7d5193f73da1bc7bd430cef6b
harshitmehta <harshitmehta155@gmail.com>
2026-04-17 11:58:01 +0530

    updated the file structure changes

---
+++ utils/data/contactsData.ts
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'harshitmehta155@gmail.com',
+    phone: '+91 9340789782',
+    address: '335, Tilak nagar , indore, Madhya Pradesh, India',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
